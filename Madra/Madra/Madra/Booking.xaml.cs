954a0c9609cdbdc8aed3eb1f5c4be758e5410ac3
Sean Kennedy <sean_jughead_lad@live.com>
2019-03-21 06:15:04 +0000

    testing

--- Madra/Madra/Madra/Booking.xaml.cs
+++ Madra/Madra/Madra/Booking.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Madra.Helper;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,22 +11,32 @@ using Xamarin.Forms.Xaml;
 
 namespace Madra
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class Booking : ContentPage
-	{
-		public Booking ()
-		{
-			InitializeComponent ();
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Booking : ContentPage
+    {
+        string selectedDate;
+        List<string> weekdays = new List<string>();
 
-            TimeSlot.Items.Add("12-1");
-            TimeSlot.Items.Add("1-2");
-            TimeSlot.Items.Add("2-3");
-            TimeSlot.Items.Add("3-4");
+        public Booking()
+        {
+            InitializeComponent();
+
+            //TimeSlot.Items.Add("12-1");
+            //TimeSlot.Items.Add("1-2");
+            //TimeSlot.Items.Add("2-3");
+            //TimeSlot.Items.Add("3-4");
+
+            getSettings();
+
         }
 
-        private void DateSelected(object sender, DateChangedEventArgs e)
+        private async void DateSelected(object sender, DateChangedEventArgs e)
         {
-            Calender.Text = "Date " +e.NewDate.ToString();
+            selectedDate = e.NewDate.ToString("yyyy-MM-dd");
+
+            //Calender.Text = "Date: " + e.NewDate.ToString();
+            Calender.Text = weekdays[0];
+
         }
 
         private async void continueBookingButton(object sender, EventArgs e)
@@ -36,6 +48,40 @@ namespace Madra
         {
             var time = TimeSlot.Items[TimeSlot.SelectedIndex];
             Time.Text = "Time " + time;
+        }      
+        
+        private async void getSettings()
+        {
+            DBConnection connection = new DBConnection();
+
+            var postData = new List<KeyValuePair<string, string>>();
+            postData.Add(new KeyValuePair<string, string>("action", "select"));
+            postData.Add(new KeyValuePair<string, string>("from", "booking_settings"));
+            postData.Add(new KeyValuePair<string, string>("where", "available = 1"));
+
+            string result = await connection.doDBConnection(postData);
+
+            dynamic dbData = JsonConvert.DeserializeObject(result);
+
+            Dictionary<int, Dictionary<string, string>> values = JsonConvert.DeserializeObject<Dictionary<int, Dictionary<string, string>>>(result);
+
+
+            for(int i = 0; i < values.Count; i++)
+            {
+                Dictionary<string, string> value = values[i];
+                weekdays.Add(value["weekday"]);
+            }
+
+            //foreach (var row in values)
+            //{
+            //    weekdays.Add(row["weekday"]);
+
+            //    foreach (var innervalue in row.Value)
+            //    {
+            //        weekdays.Add(innervalue["weekday"]);
+            //    }
+
+            //}
         }
     }
-}
+}    
