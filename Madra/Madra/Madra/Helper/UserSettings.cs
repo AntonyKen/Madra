4cd81618472ad2f5ceabb8279350dde2c99056dc
Michelle Ludwig <michelleludwig@live.com>
2019-03-21 14:20:07 +0100

    database as class login

---
+++ Madra/Madra/Madra/Helper/UserSettings.cs
@@ -0,0 +1,43 @@
+﻿using Plugin.Settings;
+using Plugin.Settings.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Madra.Helper
+{
+    public static class UserSettings
+    {
+        static ISettings AppSettings
+        {
+            get
+            {
+                return CrossSettings.Current;
+            }
+        }
+
+        public static string Email
+        {
+            get => AppSettings.GetValueOrDefault(nameof(Email), string.Empty);
+            set => AppSettings.AddOrUpdateValue(nameof(Email), value);
+        }
+
+        //public static string Password
+        //{
+        //    get => AppSettings.GetValueOrDefault(nameof(Password), string.Empty);
+        //    set => AppSettings.AddOrUpdateValue(nameof(Password), value);
+        //}
+
+        //public static string MobileNumber
+        //{
+        //    get => AppSettings.GetValueOrDefault(nameof(MobileNumber), string.Empty);
+        //    set => AppSettings.AddOrUpdateValue(nameof(MobileNumber), value);
+        //}
+
+        public static void ClearAllData()
+        {
+            AppSettings.Clear();
+        }
+
+    }
+}
--- Madra/Madra/Madra/Login.xaml.cs
+++ Madra/Madra/Madra/Login.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Madra.Helper;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,12 +21,72 @@ namespace Madra
 
         private async void loginButton(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new HomePage());
+            DBConnection connection = new DBConnection();
+            string mail = email.Text;
+            string pwd = password.Text;
+
+            if(pwd != null && mail != null )
+            {
+                var postData = new List<KeyValuePair<string, string>>();
+                postData.Add(new KeyValuePair<string, string>("action", "select"));
+                postData.Add(new KeyValuePair<string, string>("select", "app_password"));
+                postData.Add(new KeyValuePair<string, string>("from", "app_user"));
+                postData.Add(new KeyValuePair<string, string>("where", "email = '" + mail + "'"));
+
+                string result = await connection.doDBConnection(postData);
+
+                if(result != "False")
+                {
+                    string enteredPwd = "";
+
+                    dynamic dbData = JsonConvert.DeserializeObject(result);
+
+                    Dictionary<int, Dictionary<string, string>> values = JsonConvert.DeserializeObject<Dictionary<int, Dictionary<string, string>>>(result);
+                    foreach (var row in values)
+                    {
+                        foreach (var innervalue in row.Value)
+                        {
+                            enteredPwd= innervalue.Value;
+                        }
+                    }
+
+                    if(enteredPwd == pwd )
+                    {
+                        setUser(mail);
+                        await Navigation.PushAsync(new HomePage());
+                    } else
+                    {
+                        await DisplayAlert("Error", "The password is incorrect", "Ok");
+                    }
+                }
+                else
+                {
+                    await DisplayAlert("Error", "The email is incorrect", "Ok");
+                }
+
+                
+            } else
+            {
+                await DisplayAlert("Error", "Please enter all details.", "Ok");
+            }
+
+            
         }
 
         private async void forgotButton(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new ResetPassword());
         }
+
+        public static void setUser(string userEmail)
+        {
+            UserSettings.Email = userEmail;
+        }
+
+        public static string getUser()
+        {
+            return UserSettings.Email;
+        }
+
     }
 }
